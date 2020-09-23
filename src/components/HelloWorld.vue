5f51d9c8d43306d8989058b6b5624f0cf1a4ff55
hamilton-sutherland <hamiltonsutherlanda@gmail.com>
2020-09-23 11:45:18 -0700

    Init commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,60 @@
 <template>
-  <div class="hello">
+  <div class="hello container-fluid">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+        <form @submit.prevent="addTodo">
+            <label for="newTodo">Add New Item</label>
+            <input v-model="newTodo" type="text" name="newTodo" id="newTodo" value="">
+            <button type="submit" name="button" class="btn btn-primary">Add</button>
+        </form>
+        <button @click="allDone" type="button" name="button" class="btn btn-secondary">Mark all as done</button>
+        <ul>
+            <li v-for="todo in todos" :key="todo">
+                <input type="checkbox" v-model="todo.done" class="todoEl">
+                <p :class="{done: todo.done}" class="todoEl">{{todo.title}}</p> 
+                <button @click="rm(todo)" class="btn btn-secondary todoDelete">Delete</button>
+            </li>
+        </ul>
+       
+        <button @click="rmAll" type="button" name="button" class="btn btn-danger">Delete all</button>
   </div>
+    
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data () {
+    return {
+      title: "Hello friends",
+      newTodo: "",
+      todos: []
+    }
+  },
+  methods: {
+        // Add todo list item function
+        addTodo() {
+            this.todos.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = ""
+        },
+        //Remove a single todo list item
+        rm(todoToRm) {
+            let indexOf = this.todos.indexOf(todoToRm);
+            this.todos.splice(indexOf, 1);
+        },
+        //Mark all todo list items as done
+        allDone() {
+            this.todos.forEach(todo => {
+                todo.done = true;
+            });
+        },
+        //Remove all todo list items
+        rmAll() {
+            this.todos = []
+        }
+    },
   props: {
     msg: String
   }
@@ -46,13 +68,46 @@ h3 {
 }
 ul {
   list-style-type: none;
-  padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  width: 50vw;
+  max-width: 700px;
+  position: relative;
+  height: 50px;
 }
-a {
-  color: #42b983;
+form {
+  margin: 1rem;
+}
+
+form input, label, button {
+  vertical-align:middle;
+  margin: .5rem;
+}
+
+li input, p, button {
+  vertical-align:middle;
+  margin: .5rem;
+  
+}
+
+.todoEl {
+  display: inline-grid;
+  margin-right: 1rem;
+}
+
+.todoDelete {
+  position: absolute;
+  right: 0;
+}
+
+.done {
+  text-decoration: line-through;
+}
+
+.container-fluid {
+  display: grid;
+  place-items: center
 }
 </style>
